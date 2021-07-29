b658e6541796f54c341f8c82c5ce741f88d50367
Abi Gibbs <78177090+Abi-gibbs@users.noreply.github.com>
2021-07-29 22:00:58 +0000

    rounds and points working but wrong winner

--- index.js
+++ index.js
@@ -1,59 +1,59 @@
 //
 var rounds = prompt('How many rounds do you want to play?')
 
-play();
-function play(){
+
+
+let round = 0 
 let pointPlayer1 = 0 
 let pointPlayer2 = 0 
+let dice1 = Math.floor(Math.random() * 6 + 1)
+let dice2 = Math.floor(Math.random() * 6 + 1)
 
-question();
-function question(){
+do {
+
 var play = prompt ('Player1 do you want to roll the dice')
 if (play == 'yes'){
-  roll();
+   let dice1 = Math.floor(Math.random() * 6 + 1)
+  console.log('you rolled a ' + dice1)
 }
 else if (play == 'no'){
   console.log('ok bye')
 }
 else {
   console.log('invalid input please re-enter answer')
-  question();
-}}
-function roll(){
-  let dice1 = Math.floor(Math.random() * 6 + 1)
-  console.log('you rolled a ' + dice1)
+ var play = prompt ('Player1 do you want to roll the dice')
 }
 
 
 
 
-question2();
-function question2(){
 var play2 = prompt ('Player2 do you want to roll the dice')
 if (play2 == 'yes'){
-  roll1();
+ let dice2 = Math.floor(Math.random() * 6 + 1)
+  console.log('you rolled a ' + dice2)
 }
 else if (play2 == 'no'){
   console.log('ok bye')
 }
 else {
   console.log('invalid input please re-enter answer')
-  question();
-}}
-function roll1(){
-  let dice1 = Math.floor(Math.random() * 6 + 1)
-  console.log('you rolled a ' + dice1)
+  var play2 = prompt ('Player2 do you want to roll the dice')
 }
-if (roll > roll1){
+
+if (dice1 < dice2){
   console.log('player1 wins')
   pointPlayer1 ++
 }
-if (roll1 < roll) {
+else if (dice2 < dice1) {
   console.log('player2 wins')
   pointPlayer2 ++
 }
-else {
+else { 
   console.log('It is a draw')
 }
+round ++
 }
 
+while (round < rounds )
+
+console.log( 'Player1 has ' + pointPlayer1 + ' points ' +'points and Player2 has '+ pointPlayer2 + ' points' )
