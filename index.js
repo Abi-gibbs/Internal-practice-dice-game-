4af0c9ec81b35f49b0d17a2b5edca8fa4f4bb5a1
Abi Gibbs <78177090+Abi-gibbs@users.noreply.github.com>
2021-08-06 03:04:08 +0000

    working code with rounds and invalid input options

--- index.js
+++ index.js
@@ -1,43 +1,61 @@
 //
-var rounds = prompt('How many rounds do you want to play?')
+console.log('Welcome to my dice game!')
+let rounds = prompt('How many rounds do you want to play?')
 
+if (isNaN(rounds)) {
+  console.log ('Invalid input please enter a number')
+ rounds = prompt('How many rounds do you want to play?')
+
+}
 
 
 let round = 0 
 let pointPlayer1 = 0 
 let pointPlayer2 = 0 
 
-let dice1 = Math.floor(Math.random() * 6 + 1)
-let dice2 = Math.floor(Math.random() * 6 + 1)
+// let dice1 = Math.floor(Math.random() * 6 + 1)
+// let dice2 = Math.floor(Math.random() * 6 + 1)
 
 do {
 
+let dice1 = Math.floor(Math.random() * 6 + 1)
+let dice2 = Math.floor(Math.random() * 6 + 1)
 
-var play = prompt ('Player1 do you want to roll the dice')
-if (play == 'yes'){
-   let dice1 = Math.floor(Math.random() * 6 + 1)
+let play = prompt ('Player1 do you want to roll the dice')
+if (play == 'yes' || 'y' || 'ye' || 'ok'){
+  //  let dice1 = Math.floor(Math.random() * 6 + 1)
   console.log('you rolled a ' + dice1)
 }
-else if (play == 'no'){
+
+else if (play == 'no' || 'nah' || 'n' || 'no thanks'){
   console.log('ok bye')
-}
-else {
-  console.log('invalid input please re-enter answer')
- var play = prompt ('Player1 do you want to roll the dice')
+  break 
 }
 
-var play2 = prompt ('Player2 do you want to roll the dice')
-if (play2 == 'yes'){
- let dice2 = Math.floor(Math.random() * 6 + 1)
+else {
+  console.log('invalid input please re-enter answer')
+ let play = prompt ('Player1 do you want to roll the dice')
+}
+
+
+let play2 = prompt ('Player2 do you want to roll the dice')
+if (play2 == 'yes' || 'y' || 'ye' || 'ok'){
   console.log('you rolled a ' + dice2)
 }
-else if (play2 == 'no'){
+else if (play2 == 'no' || 'nah' || 'n' || 'no thanks'){
   console.log('ok bye')
+break 
 }
 else {
   console.log('invalid input please re-enter answer')
-  var play2 = prompt ('Player2 do you want to roll the dice')
+  let play2 = prompt ('Player2 do you want to roll the dice')
+if (play == 'yes' || 'y' || 'ye' || 'ok'){
+  console.log('you rolled a ' + dice2)
 }
+
+}
+round ++
+
 
 if (dice1 > dice2){
   console.log('player1 wins')
@@ -47,12 +65,12 @@ else if (dice1 < dice2) {
   console.log('player2 wins')
   pointPlayer2 ++
 }
-else if (dice1 = dice2){ 
+else { 
   console.log('It is a draw')
 }
-round ++
-}
 
-while (round < rounds )
+} while (round < rounds)
+
+
 
 console.log( 'Player1 has ' + pointPlayer1 + ' points ' +'points and Player2 has '+ pointPlayer2 + ' points' )
